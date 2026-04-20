45a83c4f884be93a55d6c479b7ef2401a45bb965
Mario Lozano Cortés <58252542+SrLozano@users.noreply.github.com>
2026-04-20 22:38:13 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            # skip non-alphanumeric characters on the left
+            while l < r and not s[l].isalnum():
+                l += 1
+            # skip non-alphanumeric characters on the right
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
